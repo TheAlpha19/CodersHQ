b2ae057aee12fc1ae42736d98ac4934ba8b2ad0c
Alpha19 <37271374+TheAlpha19@users.noreply.github.com>
2022-02-07 07:45:24 -0500

    Update admin.py
    Stager for Eventbrite Push

--- codershq/events/admin.py
+++ codershq/events/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from codershq.events.models import Event
 
-admin.site.register(Event)
+#admin.site.register(Event) #ROLLBACK to Normacy
+@admin.register(Event)	#Adding Extra codes to send request to eventbrite.
+class EventAdmin(admin.ModelAdmin):
+	def save_model(self, request, obj, form, change):	#change is bool, Request: <class 'django.core.handlers.wsgi.WSGIRequest'>, Object: <class 'codershq.events.models.Event'>
+		print("Object: {}".format(obj.short_description))	#obj is the instance of Evemt model
+		'''
+		Eventbrite conf goes here.. starting with creation
+		https://www.eventbrite.com/platform/docs/create-events
+		'''
+		super().save_model(request, obj, form, change) #saving everything
